7062528b50c1f35d135ac21ca79a355afff45b3f
sosomueller <sophia-lepges@t-online.de>
2025-07-18 08:14:15 +0200

    Update teilnehmernetzwerk.py

--- teilnehmernetzwerk.py
+++ teilnehmernetzwerk.py
@@ -34,13 +34,13 @@ for _, row in df.iterrows():
 labels = {node: f"{node}\n{data['Abteilung']} | {data['Erfahrungsstufe']}" for node, data in G.nodes(data=True)}
 
 # Graph zeichnen
-#plt.figure(figsize=(14, 10))
-#pos = nx.spring_layout(G, seed=42)
-#nx.draw_networkx_nodes(G, pos, node_color='lightgreen', node_size=800, edgecolors='lightgreen')
-#nx.draw_networkx_edges(G, pos, width=1.5, alpha=0.7)
-#nx.draw_networkx_labels(G, pos, labels, font_size=5)
+# plt.figure(figsize=(14, 10))
+# pos = nx.spring_layout(G, seed=42)
+# nx.draw_networkx_nodes(G, pos, node_color='lightgreen', node_size=800, edgecolors='lightgreen')
+# nx.draw_networkx_edges(G, pos, width=1.5, alpha=0.7)
+# nx.draw_networkx_labels(G, pos, labels, font_size=5)
 
-plt.title("Teilnehmernetzwerk basierend auf Excel-Daten", fontsize=14)
+# plt.title("Teilnehmernetzwerk basierend auf Excel-Daten", fontsize=14)
 # Pyvis Netzwerk erstellen
 net = Network(notebook=False, width="1000px", height="1800px", bgcolor="#ffffff", font_color="black")
 
